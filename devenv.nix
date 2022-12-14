4de13022e78d0607249f8d0e271cb2a7629ead22
Jashandeep Sohi <jsohi@lco.global>
2022-12-14 12:56:58 -0800

    only include linux dependencies on linux

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,6 @@
 { pkgs, ... }:
-
-{
-  # https://devenv.sh/packages/
-  packages = [
-    pkgs.git
-    pkgs.nodejs-19_x
-    pkgs.yarn
+let
+  linuxPackages = [
     pkgs.dpkg
     pkgs.fakeroot
     pkgs.rpm
@@ -14,9 +9,18 @@
     pkgs.elfutils
     pkgs.debugedit
   ];
+in {
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.nodejs-19_x
+    pkgs.yarn
+  ] ++ (if pkgs.stdenv.isLinux then linuxPackages else []);
 
   enterShell = ''
-    flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    ${if pkgs.stdenv.isLinux then ''
+      flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '' else ""}
   '';
 
 }
